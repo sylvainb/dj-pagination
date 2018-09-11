b4abaf4960b775de327b751fc3feba76ebbe2b35
Daniel <daniel.greenfeld@britecore.com>
2018-09-11 11:50:34 -0700

    Remove support for unsupported versions of Python and Django

--- setup.py
+++ setup.py
@@ -77,8 +77,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.6"
     ],
     extras_require={
         ':python_version < "3.0"': ["Django<2.0"]
